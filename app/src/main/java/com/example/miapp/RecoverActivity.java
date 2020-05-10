1bfcf2b0ace41268b2ef1c1268f173c6307de0bd
SatsuDev <wyll2985@gmail.com>
2020-05-09 19:06:36 -0500

    Adding funcionally buttons

--- app/src/main/java/com/example/miapp/RecoverActivity.java
+++ app/src/main/java/com/example/miapp/RecoverActivity.java
@@ -2,7 +2,9 @@ package com.example.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecoverActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class RecoverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recover);
+    }
+    public void Loginback (View view) {
+        Intent loginback = new Intent(this, LoginActivity.class);
+        startActivity(loginback);
+    }
+    public void Register(View view) {
+        Intent register = new Intent(this, RegisterActivity.class);
+        startActivity(register);
     }
 }
--- app/src/main/java/com/example/miapp/RegisterActivity.java
+++ app/src/main/java/com/example/miapp/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void Loginback (View view) {
+        Intent loginback = new Intent(this, LoginActivity.class);
+        startActivity(loginback);
     }
 }
